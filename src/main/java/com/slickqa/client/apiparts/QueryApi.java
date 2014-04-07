04a7445999e7fba6f1013822549fda6d18d13caa
Jason Corbett <jasoncorbett@gmail.com>
2014-04-07 13:32:27 -0600

    more documentation for the QueryApi class

--- src/main/java/com/slickqa/client/apiparts/QueryApi.java
+++ src/main/java/com/slickqa/client/apiparts/QueryApi.java
@@ -8,8 +8,16 @@ import java.util.List;
  * A simple interface allowing querying of the Slick server using information previously
  * entered into the parent (object parent, not inheritance parent).
  *
+ * @author Jason Corbett
  * Created by jcorbett on 4/6/14.
  */
 public interface QueryApi<T> {
+
+    /**
+     * Retreive a list of the specified object.
+     *
+     * @return The list of objects returned from slick.
+     * @throws SlickError if an invalid usage occurs or if the server returns an error.
+     */
     public List<T> getList() throws SlickError;
 }
