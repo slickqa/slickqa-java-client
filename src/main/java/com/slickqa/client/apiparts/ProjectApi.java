a5ab261360dbaa96e1c182c22ebb608508ed68e7
Jason Corbett <jasoncorbett@gmail.com>
2014-04-07 15:47:09 -0600

    Adding documentation for the project and release api

--- src/main/java/com/slickqa/client/apiparts/ProjectApi.java
+++ src/main/java/com/slickqa/client/apiparts/ProjectApi.java
@@ -9,7 +9,24 @@ import com.slickqa.client.model.Release;
  * Created by jcorbett on 4/6/14.
  */
 public interface ProjectApi extends CRUDApi<Project> {
+    /**
+     * Query for all the releases, no filtering.
+     */
     public QueryApi<Release> releases();
+
+    /**
+     * Use a specific release.  Use this form for create or update operations.  It will also work for get and delete
+     * operations, but it is not required.
+     *
+     * @param release The release to perform the operation on
+     */
     public ReleaseApi release(Release release);
+
+    /**
+     * Use a specific release.  You can use this form only for get or delete operations.  Using this form for create or
+     * update operations can cause an SlickApiUsageError.
+     *
+     * @param idOrName The id or name of the release.
+     */
     public ReleaseApi release(String idOrName);
 }
--- src/main/java/com/slickqa/client/apiparts/ReleaseApi.java
+++ src/main/java/com/slickqa/client/apiparts/ReleaseApi.java
@@ -8,7 +8,24 @@ import com.slickqa.client.model.Release;
  * Created by jcorbett on 4/7/14.
  */
 public interface ReleaseApi extends CRUDApi<Release> {
+    /**
+     * Query for all the builds belonging to this release, no filtering.
+     */
     public QueryApi<Build> builds();
+
+    /**
+     * Use a specific build.  Use this form for create or update operations.  It will also work for get and delete
+     * operations, but it is not required.
+     *
+     * @param build The build to use for the request
+     */
     public CRUDApi<Build> build(Build build);
+
+    /**
+     * Use a specific release.  You can use this form only for get or delete operations.  Using this form for create or
+     * update operations can cause an SlickApiUsageError.
+     *
+     * @param idOrName The id or name of the build
+     */
     public CRUDApi<Build> build(String idOrName);
 }
