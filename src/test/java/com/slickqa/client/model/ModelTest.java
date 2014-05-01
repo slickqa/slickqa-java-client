627bdcfb076a096353b1c65c75c97316ce54e953
Jason Corbett <jasoncorbett@gmail.com>
2014-05-01 16:09:53 -0600

    Adding javabean tests for the models

---
+++ src/test/java/com/slickqa/client/model/ModelTest.java
@@ -0,0 +1,42 @@
+package com.slickqa.client.model;
+
+import com.google.common.reflect.ClassPath;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static org.junit.Assert.*;
+import static com.google.code.beanmatchers.BeanMatchers.*;
+import static com.google.common.reflect.ClassPath.ClassInfo;
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.junit.runners.Parameterized.Parameters;
+import static org.junit.runners.Parameterized.Parameter;
+
+/**
+ * Tests to make sure that the models are valid, working java beans.
+ */
+@RunWith(Parameterized.class)
+public class ModelTest {
+
+    @Parameters
+    public static Collection<Object[]> parameters() throws Exception {
+        ArrayList<Object[]> params = new ArrayList<>();
+        for(ClassInfo info: ClassPath.from(Project.class.getClassLoader()).getTopLevelClasses(Project.class.getPackage().getName())) {
+            params.add(new Object[] {info.load()});
+        }
+        return params;
+    }
+
+    @Parameter
+    public Class<?> type;
+
+    @Test
+    public void beanTest() {
+        assertThat(type, allOf(hasValidBeanConstructor(),
+                hasValidGettersAndSetters(),
+                hasValidBeanToString()));
+    }
+}
